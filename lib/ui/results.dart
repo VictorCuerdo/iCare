5c604b59f7298c7ad68ca489c317b5d67e48d2d3
llherrera <95147465+llherrera@users.noreply.github.com>
2023-05-11 16:08:58 -0500

    dashboard first version

--- lib/ui/results.dart
+++ lib/ui/results.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:icare/ui/homepage.dart';
 import 'package:icare/widgets/loading_logo.dart';
 import 'package:get/get.dart';
+import 'package:webviewx/webviewx.dart';
 
 class Results extends StatefulWidget {
   const Results({Key? key}) : super(key: key);
@@ -12,6 +13,8 @@ class Results extends StatefulWidget {
 }
 
 class _ResultsState extends State<Results> {
+  late WebViewXController wvController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +28,31 @@ class _ResultsState extends State<Results> {
               Color(0xFF9BCEF3),
             ],
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: WebViewX(
+                initialContent: '<iframe title="Report Section" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNWM2YzM0YjgtOTQ0Zi00YmVlLWFhYWMtNTFiMGRjNDhjYzMzIiwidCI6ImJhYjBiNjc5LWJkNWYtNGZlOC1iNTE2LWM2YjhiMzE3Yzc4MiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>',
+                initialSourceType: SourceType.html,
+                onWebViewCreated: (controller) {
+                  wvController = controller;
+                },
+                onPageStarted: (src) {
+                  print('Page started loading: $src');
+                },
+                onPageFinished: (src) {
+                  print('Page finished loading: $src');
+                },
+                onWebResourceError: (error) {
+                  print('Error: ${error.description}');
+                },
+                width: double.infinity,
+                height: double.infinity,
+              ),
+            )
+          ],
         ),
       ),
     );
